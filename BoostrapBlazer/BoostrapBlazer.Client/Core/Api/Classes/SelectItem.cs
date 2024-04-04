57150ae82d53927f1f51b5871b7357ff13b3c0b7
Filipe Freire <filipe.freire32@gmail.com>
2024-04-04 10:20:14 +0100

    Added Rows options to paginator

---
+++ BoostrapBlazer/BoostrapBlazer.Client/Core/Api/Classes/SelectItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Core
+{
+    public class SelectItem
+    {
+        public string? Label { get; set; }
+        public object? Value { get; set; }
+        public string? StyleClass { get; set; }
+        public string? Icon { get; set; }
+        public string? Title { get; set; }
+        public bool Disabled { get; set; }
+    }
+}
--- BoostrapBlazer/BoostrapBlazer.Client/Core/Components/Table/Classes/TableLazyLoadEvent.cs
+++ BoostrapBlazer/BoostrapBlazer.Client/Core/Components/Table/Classes/TableLazyLoadEvent.cs
@@ -8,12 +8,5 @@
 		public int Rows;
 		public int First;
 		public int Last;
-
-		public TableLazyLoadEvent(int Rows, int First, int Last)
-		{
-			this.Rows = Rows;
-			this.First = First;
-			this.Last = Last;
-		}
 	}
 }
