0d6f5099c4fa9c6c1942ecfd13055bda0ba56bcf
Filipe Freire <filipe.freire32@gmail.com>
2024-04-04 01:28:56 +0100

    Added Paginator

---
+++ BoostrapBlazer/BoostrapBlazer.Client/Core/Components/Paginatior/Classes/PaginatorState.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+    namespace Core
+    {
+    public class PaginatorState
+    {
+        public int Page;
+        public int First;
+        public int Rows;
+        public int PageCount;
+    }
+}
